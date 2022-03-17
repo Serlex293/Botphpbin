88bf096c3393a844fc35aafaee49b938c875ec6e
Serlex293 <101515521+Serlex293@users.noreply.github.com>
2022-03-16 20:34:06 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,48 +16,38 @@ if($message == "/start"){
 }
 
 //Bin Lookup
-if(strpos($message, ".bin") === 0){
-    $bin = substr($message, 5);
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-    CURLOPT_URL => "https://binsu-api.vercel.app/api/".$bin,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 40,
-    CURLOPT_TIMEOUT => 10,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => [
-    "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
-    "sec-fetch-dest: document",
-    "sec-fetch-site: none",
-    "user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
-   ],
-   ]);
+if(strpos($message, ".bin") === 0)
+$result = inicio(@result);{
+Metemos la cc desde donde iniciara el conteo
 
- $result = curl_exec($curl);
- curl_close($curl);
- $data = json_decode($result, true);
- $bank = $data['data']['bank'];
- $country = $data['data']['country'];
- $brand = $data['data']['vendor'];
- $level = $data['data']['level'];
- $type = $data['data']['type'];
- $flag = $data['data']['countryInfo']['emoji'];
- $result1 = $data['result'];
+inicio = (input("Ingresa el numero de inicio: ")) 
 
+while len(inicio) != 6:                # Contabilizamos los caracteres de nuestro inicio
+    inicio += "0"                       # para añadir los digitos restantes como ceros
+ 
+inicio = int(inicio)                    # Convertimos nuestre inicio de cadena a entero para poder sumarlo
+
+print("Inicio establecido como", inicio)
+
+# Metemos el limite de la cc donde acaba el conteo
+
+limite = (input("Ingrese su limite: ")) # Establecemos 
+
+while len(limite) != 6:                # En esta parte contabilizamos los caracteres que tiene nuestro limite
+    limite += "0"                       # Para añadirle ceros restantes
+
+limite = int(limite)                    # Convertimos el digito del limite mas lo ceros que agregamos anteriormente de 
+                                        # texto o cadena a enteros para poder ir sumando 
+
+print("El limite ha sido establecido como:", limite)
+
+while inicio <= limite:                                        
+    print(".bin", inicio)
+    inicio += 1               
+                                       
     if ($result1 == true) {
     send_message($chat_id,$message_id,"***VALID BIN✅
-┏━━━━━━━━━━━━━━━━━━
-┠⌬ BIN: $bin
-┠⌬ Brand: $brand
-┠⌬ Level: $level
-┠⌬ Bank: $bank
-┠⌬ Country: $country $flag
-┠⌬ Type: $type
-┗━━━━━━━━━━━━━━━━━━
+.bin
 〄  Checked By: @$username ***");
     }
 else {
